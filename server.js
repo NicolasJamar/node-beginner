63cd86224f2cfd719a11bc2fffe1a82033c5257c
NicolasJamar <nicolas.jamar@gmail.com>
2023-03-22 10:23:43 +0100

    end raw server

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ const EventEmitter = require('events');
 class Emitter extends EventEmitter { };
 // initialize object
 const myEmitter = new Emitter();
-
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName) );
 const PORT = process.env.PORT || 3500;
 
 const serveFile = async(filePath, contentType, response) => {
   try {
-    const rawData = await fsPromises.readFile(filePath, 
+    const rawData = await fsPromises.readFile(
+      filePath, 
       !contentType.includes('image') ? 'utf8' : ''); // Not transform in utf8 if image
     const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
     response.writeHead(
-      filePath.includes('404.html') ? 400 : 200, {'Content-Type': contentType});
+      filePath.includes('404.html') ? 404 : 200, {'Content-Type': contentType});
     response.end(
       contentType === 'application/json' ? JSON.stringify(data) : data
     );
@@ -99,6 +100,3 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-myEmitter.on('log', (msg) => logEvents(msg) );
