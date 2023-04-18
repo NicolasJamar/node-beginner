1b9a77f638323d72cab802e4ee86d56e5c31b647
NicolasJamar <nicolas.jamar@gmail.com>
2023-04-18 18:43:58 +0200

    thunder client

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 // routes
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 
 // Manage 404
