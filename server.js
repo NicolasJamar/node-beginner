8b78c3ba240defc686a35f0b674ecdc46376f48b
NicolasJamar <nicolas.jamar@gmail.com>
2023-04-19 16:25:57 +0200

    begin MVC API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
@@ -10,18 +11,6 @@ const PORT = process.env.PORT || 3500;
 app.use(logger)
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'https://yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
-const corsOptions = {
-  origin: (origin, callback) => {
-    // if the domain is in the whitelist OR No origin
-    if(whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true) // the first argument is the error, the 2nd allowed the origin
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
@@ -34,11 +23,9 @@ app.use(express.json());
 
 // serve static files (css, img..)
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 
