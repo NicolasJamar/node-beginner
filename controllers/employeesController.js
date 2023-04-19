67fdc39fd40f87f6582546f4d5de5b8a5c7102b6
NicolasJamar <nicolas.jamar@gmail.com>
2023-04-19 17:01:25 +0200

    MVC API: modify employeesController

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,6 +1,6 @@
 const data = {
-  employees: require('../model/employees.json')
-  //setEmployees: function(data) { this.employees=data }
+  employees: require('../model/employees.json'),
+  setEmployees: function(data) { this.employees=data }
 };
 
 const getAllEmployees = (req, res) => {
@@ -8,17 +8,26 @@ const getAllEmployees = (req, res) => {
 }
 
 const createEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname
-  })
+  const newEmployee = {
+    id: data.employees[data.employees.length - 1].id + 1 || 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+  }
+
+  if(!newEmployee.firstname || !newEmployee.lastname) {
+    return res.status(400).json({ 'message': 'First & lastname are required' })
+  }
+
+  data.setEmployees([...data.employees, newEmployee])
+  res.status(201).json(data.employees)
 }
 
 const updateEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname
-  })
+  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+
+  if(!employee) {
+    return res.status(400).json({"message": `Employee ID ${req.body.id} not found.`})
+  }
 }
 
 const deleteEmployee = (req, res) => {
