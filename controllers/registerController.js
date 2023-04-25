118a27c9bdeda233725ee7f954d75c9957cc2c89
NicolasJamar <nicolas.jamar@gmail.com>
2023-04-25 09:53:42 +0200

    Auth: begin

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,6 +6,7 @@ const usersDB = {
 const fsPromises = require('fs').promises;
 const path = require('path');
 const bcrypt = require('bcrypt');
+const { log } = require('console');
 
 const handleNewUser = async (req, res) => {
   const { user, pwd } = req.body;
@@ -15,16 +16,26 @@ const handleNewUser = async (req, res) => {
 
   // Check for duplicate usernames in the db
   const duplicate = usersDB.users.find( person => person.username === user)
-  if(duplicate) return res.sendStatus(409); // Conflict
+  if(duplicate) return res.sendStatus(409); // conflict
   try {
-    //encrypt the password
+    // encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10)
     const newUser = {
       "username": user,
       "password": hashedPwd
     }
-    usersDB.setUsers([...usersDB.users, newUser])
+
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', 'model', 'users.json'),
+      JSON.stringify(usersDB.users)
+    )
+    console.log(usersDB.users);
+    res.status(201).json({'success': `New user ${user} was created!`})
+
   } catch(err) {
     res.status(500).json({'message': err.message})
   }
 }
+
+module.exports = { handleNewUser }
