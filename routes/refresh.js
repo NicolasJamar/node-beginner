b9d2066ec51e781f4159ba012ff35f5bd195959b
NicolasJamar <nicolas.jamar@gmail.com>
2023-05-05 15:20:30 +0200

    JWT: refresh route

--- routes/refresh.js
+++ routes/refresh.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const refreshTokenController = require('../controllers/refreshTokenController');
 
-router.post('/', authController.handleLogin);
+router.get('/', refreshTokenController.handleRefreshToken);
 
 module.exports = router;
