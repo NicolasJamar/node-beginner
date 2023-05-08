b8c4145abd5935aaf032d0ee190560043a9cb03b
NicolasJamar <nicolas.jamar@gmail.com>
2023-05-08 10:45:51 +0200

    User roles: midware verify JWT

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -23,7 +23,11 @@ const handleRefreshToken = (req, res) => {
       if(err || foundUser.username !== decoded.username) return res.sendStatus(403); // invalid token
       const roles = Object.values(foundUser.roles)
       const accessToken = jwt.sign(
-        {"username": decoded.username},
+        { "UserInfo" : {
+            "username": decoded.username,
+            "roles": roles
+          }
+        },
         process.env.ACCESS_TOKEN_SECRET,
         {expiresIn: '30s'}
       )
