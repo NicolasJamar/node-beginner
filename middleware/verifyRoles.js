efd9fc3dcaea23bcd04272b2529a6e5678827fd7
NicolasJamar <nicolas.jamar@gmail.com>
2023-05-10 15:17:59 +0200

    User roles: add role to route employees

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -4,8 +4,12 @@ const verifyRoles = (...allowedRoles) => {
     const rolesArray = [...allowedRoles];
     console.log(rolesArray);
     console.log(req.roles);
+    // includes return a boolean, so here an array of booleans
+    // .find() : we need to find if a role has access right, so if it's "true"
     const result = req.roles.map(role => rolesArray.includes(role)).find(val => val === true);
     if(!result) return res.sendStatus(401);
     next(); 
   }
 }
+
+module.exports = verifyRoles
