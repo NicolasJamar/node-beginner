48e189a5d5b4f0032a4831b5b6adbd2fb82e6f68
NicolasJamar <nicolas.jamar@gmail.com>
2023-06-20 10:39:26 +0200

    add react localhost:3000

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   'https://www.yoursite.com', 
   'https://yoursite.com', 
   'http://127.0.0.1:5500', 
-  'http://localhost:3500'
+  'http://localhost:3500',
+  'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
