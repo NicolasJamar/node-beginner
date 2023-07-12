95962698f9944b8e8d3c6d285ec24117cb58616b
NicolasJamar <nicolas.jamar@gmail.com>
2023-07-12 21:56:36 +0200

    jwt rotation in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const handleLogin = async(req, res) => {
+  const cookies = req.cookies;
   const { user, pwd } = req.body;
   if(!user || !pwd ){
     res.status(400).json({ 'message': 'Username and password are required' })
@@ -24,18 +25,29 @@ const handleLogin = async(req, res) => {
       { expiresIn: '10m' }
     );
     // the refreshToken is only there to verify that you can get a new accessToken
-    const refreshToken = jwt.sign(
+    const newRefreshToken = jwt.sign(
       { "username": foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: '1d' }
     );
+
+    const newRefreshTokenArray = 
+      !cookies?.jwt
+        ? foundUser.refreshToken
+        : foundUser.refreshToken.filter( rt => rt !== cookies.jwt)
+    
+    //we delete the old cookie on every auth
+    if(cookies?.jwt) {
+      res.clearCookie('jwt', { httpOnly: true, sameSite: "None", secure: true })
+    }
+
     // Saving refreshToken with current user
-    foundUser.refreshToken = refreshToken;
+    foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
     const result = await foundUser.save();
     console.log(result);
-      // secure: true,
-    //we store the refreshToken in a cookie httpOnly
-    res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: "None", maxAge: 24*60*60*1000 }) //with httpOnly, the cookie is not available by the JS
+    // secure: true,
+    //we store the newRefreshToken in a cookie httpOnly
+    res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: "None", maxAge: 24*60*60*1000 }) //with httpOnly, the cookie is not available by the JS
     res.json({ accessToken })
   } else {
     res.sendStatus(401).json({'message': err.message})
